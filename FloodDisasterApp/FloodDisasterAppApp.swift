1905bc6bf34af2c1c4a8efe8aff5ed878c1396ea
pveda1 <parulveda1@gmail.com>
2021-03-06 14:55:59 -0800

    Initial Commit

---
+++ FloodDisasterApp/FloodDisasterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloodDisasterAppApp.swift
+//  FloodDisasterApp
+//
+//  Created by Parul Veda on 3/6/21.
+//
+
+import SwiftUI
+
+@main
+struct FloodDisasterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
